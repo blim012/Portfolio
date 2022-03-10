62207b22d061d44830468011c70f7d70de3f9c50
Brandon Lim <brandonlimchi@gmail.com>
2022-03-10 03:26:16 -0800

    add Portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Portfolio = (props) => {
+  return (
+    <main id="portfolio">
+      <section id="welcome">
+        <h1>Welcome</h1>
+      </section>
+      <section id="projects">
+        <h1>Projects</h1>
+      </section>
+      <section id="skills">
+        <h1>Skills</h1>
+      </section>
+      <section id="contacts">
+        <h1>Contact</h1>
+      </section>
+    </main>
+  )
+};
+
+export default Portfolio;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Portfolio from './components/Portfolio.js';
 import R3F from './components/R3F.js';
 import './stylesheets/reset.css';
 import './stylesheets/style.css';
+import './stylesheets/welcome.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <R3F />
+    <>
+      <R3F />
+      <Portfolio />
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
