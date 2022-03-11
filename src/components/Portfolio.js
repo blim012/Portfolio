720f9dfb83e9db828e8d0c52a700f07418dfd35c
Brandon Lim <brandonlimchi@gmail.com>
2022-03-10 16:17:36 -0800

    implement changing text section banner

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,59 @@
 import React from "react";
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const Portfolio = (props) => {
+  useEffect(() => {
+    ScrollTrigger.create({
+      start: 'top top',
+      endTrigger: '#projects',
+      end: 'top top',
+      onEnterBack: () => changeBannerText("Hello, I'm Brandon")
+    });
+    
+    ScrollTrigger.create({
+      trigger: '#projects', 
+      start: 'top top',
+      endTrigger: '#skills',
+      end: 'top top',
+      onEnter: () => changeBannerText('Some projects I made'),
+      onEnterBack: () => changeBannerText('Some projects I made')
+    });
+    
+    ScrollTrigger.create({
+      trigger: '#skills', 
+      start: 'top top',
+      endTrigger: '#contacts',
+      end: 'top top',
+      onEnter: () => changeBannerText("Here are some things I'm good at"),
+      onEnterBack: () => {
+        console.log('here')
+        changeBannerText("Here are some things I'm good at")
+      }
+    });
+    
+    ScrollTrigger.create({
+      trigger: '#contacts', 
+      start: 'top top',
+      end: 'bottom bottom',
+      onEnter: () => changeBannerText("Let's Talk!")
+    });
+    
+    function changeBannerText(text) {
+      gsap.to('.banner-text', {
+        duration: 1,
+        text: text
+      });
+    }
+  }, [])
+
   return (
     <main id="portfolio">
-      <section id="welcome">
-        <h1>Welcome</h1>
-      </section>
+      <header className="section-banner">
+        {'> '}<span className="banner-text">Hello, I'm Brandon</span>
+      </header>
+      <section id="welcome"></section>
       <section id="projects">
         <h1>Projects</h1>
         <article className="project">
