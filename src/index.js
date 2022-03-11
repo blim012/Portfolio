587703a1c26cbce57f9b932986bdb80ea9dcfce1
Brandon Lim <brandonlimchi@gmail.com>
2022-03-10 16:17:07 -0800

    rename welcome css to portfolio css

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Portfolio from './components/Portfolio.js';
 import R3F from './components/R3F.js';
 import gsap from 'gsap';
 import ScrollTrigger from "gsap/ScrollTrigger";
+import TextPlugin from 'gsap/TextPlugin';
 
 import './stylesheets/reset.css';
 import './stylesheets/style.css';
-import './stylesheets/welcome.css';
+import './stylesheets/portfolio.css';
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 ReactDOM.render(
   <React.StrictMode>
